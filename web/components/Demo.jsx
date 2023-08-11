750d3dc9cef4325290b4bf974e8f04f02c5a9360
thisisrahmat <thisisrahmat@gmail.com>
2023-08-11 21:20:56 +0100

    got the design styles clickable and exchangable with the deisgns in the table, now need to fix sizing'

--- web/components/Demo.jsx
+++ web/components/Demo.jsx
@@ -33,12 +33,12 @@ const designStyles = [
   {
     title: 'Mid-century modern',
     photos: [
-      'midcentury_1.png',
-      'midcentury_2.png',
-      'midcentury_3.png',
-      'midcentury_4.png',
-      'midcentury_5.png',
-      'midcentury_6.png',
+      'mid-century_modern_1.png',
+      'mid-century_modern_2.png',
+      'mid-century_modern_3.png',
+      'mid-century_modern_4.png',
+      'mid-century_modern_5.png',
+      'mid-century_modern_6.png',
     ],
     video: 'midcentury_demo.mp4',
   },
@@ -64,62 +64,14 @@ export default function Demo() {
     setSelectedStyle(style === selectedStyle ? null : style);
   };
 
+    // Find the selected design style based on the selectedStyle
+    const selectedDesignStyle = designStyles.find((style) => style.title === selectedStyle);
+
+
   return (
     <div className="relative flex justify-center flex-row space-x-3"> {/* Add space-x-10 for gap */}
 
 {/* List of Design Styles */}
-{/* <div className="sm:space-y-2 w-1/3 sm:text-right text-left text-[13.13px] sm:text-[19.04px]">
-        <div className="rounded-xl p-1 sm:-mt-1">
-   
-            <button className="uppercase sm:text-[19.04px] text-[#D4BBF8] rounded-xl  p-1 sm:-mt-1 bg-[#004F30] font-semibold">Contemporary</button>
-
-        </div>
-        <div className=" text-[#004F30] font-semibold uppercase">
-          <button  className="uppercase sm:text-[19.04px] text-[#004F30] rounded-xl  p-1 sm:-mt-1  font-semibold">Scandinavian</button>
-        </div>
-        <div className=" text-[#004F30] font-semibold uppercase">
-          <button className="uppercase sm:text-[19.04px] text-[#004F30] rounded-xl p-1 sm:-mt-1 -ml-10 font-semibold" style={{ whiteSpace: 'nowrap' }}>
-            Mid-century modern
-          </button>
-        </div>
-        <div className=" text-[#004F30] font-semibold uppercase">
-          <button  className="uppercase sm:text-[19.04px] text-[#004F30]rounded-xl  p-1 sm:-mt-1  font-semibold">Coastal</button>
-        </div>
-
-      </div> */}
-
-{/* {designStyles.map((title, design_pics, design_videos)) => (
-
-  <table className="w-full border-collapse rounded-l overflow-hidden sm:text-[12.23px] text-[4.9px] text-[#004F30]" style={{  background: 'white' }}>
-  <tbody>
-    <tr>
-      <td className="border-[2px] border-green-700 p-4" style={{ height: '150px', width: '200px' }}>
-        </table>
-        </tbody>
-        </tr>
-        </td>
-)
-
-} */}
-
-  {/* List of Design Styles */}
-  {/* <div className="sm:space-y-2 w-1/3 text-right text-[13.13px] sm:text-[19.04px]">
-  {designStyles.map((style) => (
-    <div key={style.title}>
-      <button
-        className={`uppercase sm:text-[19.04px] p-1 sm:-mt-1 -ml-10  ${
-          selectedStyle === style.title
-            ? 'text-[#D4BBF8] font-semibold bg-[#004F30] rounded-xl p-1 sm:pl-2 pr-2'
-            : 'text-[#004F30] font-semibold p-1 sm:pl-2 pr-2'
-        }`}
-        style={{ whiteSpace: 'nowrap' }}
-        onClick={() => handleStyleClick(style.title)}
-      >
-        {style.title}
-      </button>
-    </div>
-  ))}
-</div> */}
 
 <div className="sm:space-y-2 w-1/3 text-right text-[13.13px] sm:text-[19.04px]">
   {designStyles.map((style) => (
@@ -160,93 +112,38 @@ export default function Demo() {
       {/* <div className="sm:w-1/3 border-[2px] rounded-lg overflow-hidden border-green-700 mt-4">
         <table className="w-full border-collapse rounded-l overflow-hidden sm:text-[12.23px] text-[4.9px] text-[#004F30]" style={{ height: 'calc(100vh - 150px)', background: 'white' }}>
           <tbody> */}
-      {/* Table with Interior Projects */}
-      <div class="sm:border-[2px] rounded-lg overflow-hidden mb-10 border-green-700 -mt-2 ">
-        <table className="w-full border-collapse rounded-l overflow-hidden sm:text-[12.23px] text-[4.9px] text-[#004F30]" style={{  background: 'white' }}>
-          <tbody>
-            <tr>
-              <td className="border-[2px] border-green-700 p-4" style={{ height: '150px', width: '200px' }}>
-                {/* Add border class and padding */}
-                <div className="text-center">
-                  <Image
-                    src="/contemporary/contemporary_1.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                  />
-               <p>Interior Title $200</p>
-                </div>
-              </td>
-              <td className="border-[2px] border-green-700 p-4" style={{ height: '150px', width: '200px' }}>
-                {/* Add border class and padding */}
-                <div className="text-center">
-                  <Image
-                    src="/contemporary/contemporary_2.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                  />
-                  <p>Interior Title $200</p>
-          
-                </div>
-              </td>
-            </tr>
+    {/* Table with Interior Projects */}
+  {/* Table with Interior Projects */}
+  <div className="sm:border-[2px] rounded-lg overflow-hidden mb-10 border-green-700 -mt-2 ">
+      <table className="w-full border-collapse rounded-l overflow-hidden sm:text-[12.23px] text-[4.9px] text-[#004F30]" style={{ background: 'white' }}>
+        <tbody>
+          {selectedDesignStyle &&
+            [...Array(3)].map((_, rowIndex) => (
+              <tr key={rowIndex}>
+                {[0, 1].map((colIndex) => {
+                  const photoIndex = rowIndex * 2 + colIndex;
+                  const photo = selectedDesignStyle.photos[photoIndex];
+                  return (
+                    <td
+                      key={colIndex}
+                      className="border-[2px] border-green-700 p-4"
+                      style={{ height: '150px', width: '200px' }}
+                    >
+                      {photo && (
+                        <div className="text-center">
+                          <Image src={`/${selectedStyle}/${photo}`} width={500} height={500} alt="Interior" />
+                          <p>Interior Title $200</p>
+                        </div>
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
 
-            <tr>
-              <td className="border-[2px] border-green-700 p-4"  style={{ height: '150px', width: '200px' }}>
-                {/* Add border class and padding */}
-                <div className="text-center">
-                  <Image
-                    src="/contemporary/contemporary_3.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                  />
-                 <p>Interior Title $200</p>
-                </div>
-              </td>
-              <td className="border-[2px] border-green-700 p-4">
-                {/* Add border class and padding */}
-                <div className="text-center">
-                  <Image
-                    src="/contemporary/contemporary_4.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                  />
-                 <p>Interior Title $200</p>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="border-[2px] border-green-700 p-4"  style={{ height: '100px', width: '200px' }}>
-                {/* Add border class and padding */}
-                <div className="text-center align-text-bottom">
-                  <Image
-                    src="/contemporary/contemporary_5.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                  />
-                 <p>Interior Title $200</p>
-                </div>
-              </td>
-              <td className="border-[2px] border-green-700 p-4">
-                {/* Add border class and padding */}
-                <div className="text-center">
-                  <Image
-                    src="/contemporary/contemporary_6.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                  />
-                <p>Interior Title $200</p>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
     </div>
   
         
