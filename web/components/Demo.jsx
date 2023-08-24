ce7f13482c2f0f23d2e2df3364ad6ead4fa93cdf
thisisrahmat <thisisrahmat@gmail.com>
2023-08-25 00:25:41 +0100

    reverted back to old naming

--- web/components/Demo.jsx
+++ web/components/Demo.jsx
@@ -4,31 +4,22 @@ import Image from "next/image";
 import Link from 'next/link';
 import { useState } from 'react';
 
-import Phone  from '../assets/rectangle.svg';
+
 import VideoDemo from './VideoDemo'
 import Spline from '@splinetool/react-spline';
 
 
 const designStyles = [
   {
-    // title: 'Contemporary',
-    // photos: [
-    //   'contemporary_1.png',
-    //   'contemporary_2.png',
-    //   'contemporary_3.png',
-    //   'contemporary_4.png',
-    //   'contemporary_5.png',
-    //   'contemporary_6.png',
-    // ],
-    title: 'Farmhouse',
+  title: 'Contemporary',
     photos: [
-      'farmhouse_1.png',
-      'farmhouse_2.png',
-      'farmhouse_3.png',
-      'farmhouse_4.png',
-      'farmhouse_5.png',
-      'farmhouse_6.png',
-    ],
+    'contemporary_1.png',
+     'contemporary_2.png',
+   'contemporary_3.png',
+   'contemporary_4.png',
+   'contemporary_5.png',
+  'contemporary_6.png',
+     ],
     photo_titles: [
       'West Elm $1399',
       'H&M $31.99',
